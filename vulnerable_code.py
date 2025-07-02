b32741a1f6593b94084e089c5f31bd4ec1d19729
spm-ai-labs <sebastianpmenendez@gmail.com>
2025-07-01 22:02:39 -0300

    Update vulnerable_code.py
    nuevos cambios, elimina comentarios

--- vulnerable_code.py
+++ vulnerable_code.py
@@ -2,8 +2,7 @@
 import os
 
 def get_file_content(filename):
-    # VIOLACIÓN OWASP: Path Traversal
-    # Un atacante podría usar '..' para navegar por el sistema de archivos.
+   
     full_path = "/var/www/data/" + filename
     with open(full_path, 'r') as f:
         return f.read()
