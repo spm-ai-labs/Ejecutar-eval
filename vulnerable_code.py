92c6a31563289c87f10b474383a804b09e164d72
spm-ai-labs <sebastianpmenendez@gmail.com>
2025-07-23 16:54:17 -0300

    Update vulnerable_code.py

--- vulnerable_code.py
+++ vulnerable_code.py
@@ -1,12 +1,1 @@
 # test_code.py
-import os
-
-API_GOOGLE="psd9902DDSfd72923"
-passord=10291
-def get_file_content(filename):
-   
-    full_path = "/var/www/data/" + filename
-    with open(full_path, 'r') as f:
-        return f.read()
-
-print("Función de prueba lista.")
